34991254a35d7ac8bb72aecf6d6cf6361bc64f4c
Hampus Adamsson <adamsson.hampus@gmail.com>
2015-10-26 14:01:24 +0100

    flask

--- C3/app/test.py
+++ C3/app/test.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    file = open("result.txt", "r")
+    lines = file.readlines()
+    return lines[0]
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
+
